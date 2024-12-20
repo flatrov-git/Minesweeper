9441dae2f8ce044716611532bf672c5dc748d7f6
flatrov <flatrov1@gmail.com>
2024-12-20 15:43:50 +0330

    coding createBoardGame function

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,36 @@
 "use strict";
 let $ = document;
-let backEndBoard = [];
+let size = 10;
+let levelGameOption = "easy";
+let numberOfBombs = 10;
+let handlerLevelGameOption = $.getElementById("level-game");
+handlerLevelGameOption.addEventListener('change', (data) => {
+    levelGameOption = handlerLevelGameOption.value;
+});
+switch (levelGameOption) {
+    case "easy":
+        numberOfBombs = 10;
+        break;
+    case "medium":
+        numberOfBombs = 35;
+        break;
+    case "hard":
+        numberOfBombs = 50;
+        break;
+    default:
+        numberOfBombs = 10;
+        break;
+}
+function createBoardGame() {
+    let column = Array(size).fill(null).map(_ => Array(size).fill(false));
+    for (let i = 0; numberOfBombs > i; i++) {
+        let randNumberX = Math.round(Math.random() * (size - 1));
+        let randNumberY = Math.round(Math.random() * (size - 1));
+        if (!column[randNumberX][randNumberY])
+            column[randNumberX][randNumberY] = true;
+        else
+            i--;
+    }
+    return column;
+}
+console.log(createBoardGame());
--- src/app.ts
+++ src/app.ts
@@ -2,5 +2,45 @@
 
 let $ = document
 
-let backEndBoard: [boolean][] = []
+let size: number = 10
+let levelGameOption: string = "easy"
+let numberOfBombs: number = 10
 
+let handlerLevelGameOption = $.getElementById("level-game") as HTMLSelectElement
+
+handlerLevelGameOption.addEventListener('change', (data: Event) => {
+    levelGameOption = handlerLevelGameOption.value
+})
+
+switch (levelGameOption) {
+    case "easy":
+        numberOfBombs = 10
+        break
+    case "medium":
+        numberOfBombs = 35
+        break
+    case "hard":
+        numberOfBombs = 50
+        break
+    default:
+        numberOfBombs = 10
+        break
+}
+
+function createBoardGame() {
+    let column = Array(size).fill(null).map(_ => Array(size).fill(false))
+
+    for (let i = 0; numberOfBombs > i; i++) {
+        let randNumberX: number = Math.round(Math.random() * (size - 1))
+        let randNumberY: number = Math.round(Math.random() * (size - 1))
+
+        if(!column[randNumberX][randNumberY])
+            column[randNumberX][randNumberY] = true
+        else
+            i--
+    }
+
+    return column
+}
+
+console.log(createBoardGame())
