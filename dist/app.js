a0c216c508ea31129d3f401e617ac259faa48b57
flatrov <flatrov1@gmail.com>
2024-12-20 16:53:47 +0330

    add function createFrontBoardGame and numberOfBombsPerHouse

--- dist/app.js
+++ dist/app.js
@@ -2,35 +2,64 @@
 let $ = document;
 let size = 10;
 let levelGameOption = "easy";
-let numberOfBombs = 10;
+let numberOfBombs = 25;
 let handlerLevelGameOption = $.getElementById("level-game");
 handlerLevelGameOption.addEventListener('change', (data) => {
     levelGameOption = handlerLevelGameOption.value;
 });
-switch (levelGameOption) {
-    case "easy":
-        numberOfBombs = 10;
-        break;
-    case "medium":
-        numberOfBombs = 35;
-        break;
-    case "hard":
-        numberOfBombs = 50;
-        break;
-    default:
-        numberOfBombs = 10;
-        break;
-}
-function createBoardGame() {
-    let column = Array(size).fill(null).map(_ => Array(size).fill(false));
+function createMainBoardGame() {
+    switch (levelGameOption) {
+        case "easy":
+            numberOfBombs = 25;
+            break;
+        case "medium":
+            numberOfBombs = 35;
+            break;
+        case "hard":
+            numberOfBombs = 50;
+            break;
+        default:
+            numberOfBombs = 10;
+            break;
+    }
+    let mainBoardGame = Array(size).fill(null).map(_ => Array(size).fill(false));
     for (let i = 0; numberOfBombs > i; i++) {
         let randNumberX = Math.round(Math.random() * (size - 1));
         let randNumberY = Math.round(Math.random() * (size - 1));
-        if (!column[randNumberX][randNumberY])
-            column[randNumberX][randNumberY] = true;
+        if (!mainBoardGame[randNumberX][randNumberY])
+            mainBoardGame[randNumberX][randNumberY] = true;
         else
             i--;
     }
-    return column;
+    return mainBoardGame;
 }
-console.log(createBoardGame());
+function createFrontBoardGame() {
+    let frontBoardGame = Array(size).fill(null).map(_ => Array(size).fill("*"));
+    let mainBoardGame = createMainBoardGame();
+    for (let i = 0; size > i; i++) {
+        for (let j = 0; size > j; j++) {
+            if (mainBoardGame[i][j])
+                frontBoardGame[i][j] = "#";
+            else
+                frontBoardGame[i][j] = numberOfBombsPerHouse(i, j, mainBoardGame);
+        }
+    }
+    return frontBoardGame;
+}
+function numberOfBombsPerHouse(i, j, mainBoardGame) {
+    let bombs = 0;
+    for (let x = -1; 1 >= x; ++x) {
+        for (let y = -1; 1 >= y; ++y) {
+            let nx = i + x;
+            let ny = j + y;
+            if (nx >= 0 && ny >= 0 && nx < size && ny < size && mainBoardGame[nx][ny]) {
+                bombs++;
+            }
+        }
+    }
+    if (bombs)
+        return String(bombs);
+    else
+        return " ";
+}
+console.log(createFrontBoardGame());
--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ let $ = document
 
 let size: number = 10
 let levelGameOption: string = "easy"
-let numberOfBombs: number = 10
+let numberOfBombs: number = 25
 
 let handlerLevelGameOption = $.getElementById("level-game") as HTMLSelectElement
 
@@ -12,35 +12,72 @@ handlerLevelGameOption.addEventListener('change', (data: Event) => {
     levelGameOption = handlerLevelGameOption.value
 })
 
-switch (levelGameOption) {
-    case "easy":
-        numberOfBombs = 10
-        break
-    case "medium":
-        numberOfBombs = 35
-        break
-    case "hard":
-        numberOfBombs = 50
-        break
-    default:
-        numberOfBombs = 10
-        break
-}
+function createMainBoardGame() {
+    switch (levelGameOption) {
+        case "easy":
+            numberOfBombs = 25
+            break
+        case "medium":
+            numberOfBombs = 35
+            break
+        case "hard":
+            numberOfBombs = 50
+            break
+        default:
+            numberOfBombs = 10
+            break
+    }
 
-function createBoardGame() {
-    let column = Array(size).fill(null).map(_ => Array(size).fill(false))
+    let mainBoardGame: boolean[][] = Array(size).fill(null).map(_ => Array(size).fill(false))
 
     for (let i = 0; numberOfBombs > i; i++) {
         let randNumberX: number = Math.round(Math.random() * (size - 1))
         let randNumberY: number = Math.round(Math.random() * (size - 1))
 
-        if(!column[randNumberX][randNumberY])
-            column[randNumberX][randNumberY] = true
+        if (!mainBoardGame[randNumberX][randNumberY])
+            mainBoardGame[randNumberX][randNumberY] = true
         else
             i--
     }
 
-    return column
+    return mainBoardGame
 }
 
-console.log(createBoardGame())
+
+function createFrontBoardGame() {
+    let frontBoardGame: string[][] = Array(size).fill(null).map(_ => Array(size).fill("*"))
+    let mainBoardGame: boolean[][] = createMainBoardGame()
+
+    for (let i = 0; size > i; i++) {
+        for (let j = 0; size > j; j++) {
+            if (mainBoardGame[i][j])
+                frontBoardGame[i][j] = "#"
+            else
+                frontBoardGame[i][j] = numberOfBombsPerHouse(i, j, mainBoardGame)
+        }
+    }
+
+    return frontBoardGame
+}
+
+function numberOfBombsPerHouse(i: number, j: number, mainBoardGame: boolean[][]): string {
+    let bombs = 0;
+
+    for (let x = -1; 1 >= x; ++x) {
+        for (let y = -1; 1 >= y; ++y) {
+            let nx = i + x
+            let ny = j + y
+
+            if(nx >= 0 && ny >= 0 && nx < size && ny < size && mainBoardGame[nx][ny]){
+                bombs++
+            }
+        }
+    }
+
+    if (bombs)
+        return String(bombs)
+    else
+        return " ";
+}
+
+console.log(createFrontBoardGame())
